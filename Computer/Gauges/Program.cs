59519cf43fae12f7e9490a44925d8c6409c34943
halojenproductions <halojenproductions@users.noreply.github.com>
2018-08-26 00:40:55 +1200

    Finished ram usage meter.

--- Computer/Gauges/Program.cs
+++ Computer/Gauges/Program.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Microsoft.VisualBasic.Devices;
 
 namespace Gauges {
 	class Program {
 		public static String ComPort { get; set; }
 		public static List<Counter> counters = new List<Counter>();
 		static StringBuilder SB = new StringBuilder();
+		static int iTotalRam;
 
 		static void Main(string[] args) {
 			ComPort = "";
 			foreach (string a in args) {
-				if (a.Substring(0, 1) != "/") {
+				if (a.Substring(0, 1) != "/" && a.Substring(0, 1) != "-") {
 					ComPort = a.ToString();
 				}
 			}
@@ -26,11 +28,13 @@ namespace Gauges {
 				Console.WriteLine("Port: " + ComPort);
 			}
 
-			counters.Add(new Counter(new PerformanceCounter("Processor", "% Processor Time", "0")));
-			counters.Add(new Counter(new PerformanceCounter("Processor", "% Processor Time", "1")));
-			counters.Add(new Counter(new PerformanceCounter("Processor", "% Processor Time", "2")));
-			counters.Add(new Counter(new PerformanceCounter("Processor", "% Processor Time", "3")));
-			counters.Add(new Counter(new PerformanceCounter("Memory", "Available MBytes")));
+			counters.Add(new Counter(Counter.CounterTypes.CPU, new PerformanceCounter("Processor", "% Processor Time", "0")));
+			counters.Add(new Counter(Counter.CounterTypes.CPU, new PerformanceCounter("Processor", "% Processor Time", "1")));
+			counters.Add(new Counter(Counter.CounterTypes.CPU, new PerformanceCounter("Processor", "% Processor Time", "2")));
+			counters.Add(new Counter(Counter.CounterTypes.CPU, new PerformanceCounter("Processor", "% Processor Time", "3")));
+			counters.Add(new Counter(Counter.CounterTypes.RAM, new PerformanceCounter("Memory", "Available MBytes")));
+
+			iTotalRam = (int)(new ComputerInfo().TotalPhysicalMemory / 1024 / 1024);
 
 			Console.WriteLine("Running.");
 
@@ -41,7 +45,6 @@ namespace Gauges {
 					} catch (Exception ex) {
 						Console.WriteLine("Couldn't open COM port.");
 						System.Threading.Thread.Sleep(5000);
-
 					}
 					while (true) {
 						for (int i = 0; i < counters.Count; i++) {
@@ -76,37 +79,49 @@ namespace Gauges {
 			}
 		}
 		public class Counter {
+			public enum CounterTypes { CPU, RAM }
 			public PerformanceCounter PerfCounter { get; set; }
 			private int[] _ShortBuffer = new int[2];
 			private int[] _LongBuffer = new int[50];
 			public int ShortAverage { get; set; }
 			public int LongAverage { get; set; }
+			private CounterTypes CounterType;
 
-			public Counter(PerformanceCounter PC) {
+			public Counter(CounterTypes CT, PerformanceCounter PC) {
 				ShortAverage = 0;
 				LongAverage = 0;
+				CounterType = CT;
 				PerfCounter = PC;
 			}
 
 			public void GetNextPlot() {
-				int iNewVal = (int)PerfCounter.NextValue();
+				int iNewVal = 0;
+				switch (CounterType) {
+					case CounterTypes.CPU:
+						iNewVal = (int)PerfCounter.NextValue();
+						break;
+					case CounterTypes.RAM:
+						int iUsed = iTotalRam - (int)PerfCounter.NextValue();
+						iNewVal = (int)((float)iUsed / (float)iTotalRam * (float)100); // Convert to percentage.
+						break;
+				}
 
-				// Add new value to short buffer.
+				// Add new value to the short buffer.
 				for (int i = 0; i < _ShortBuffer.Length - 1; i++) {
 					_ShortBuffer[i] = _ShortBuffer[i + 1];
 				}
 				_ShortBuffer[_ShortBuffer.Length - 1] = iNewVal;
 
-				// Add new value to long buffer.
+				// Add new value to the long buffer.
 				for (int i = 0; i < _LongBuffer.Length - 1; i++) {
 					_LongBuffer[i] = _LongBuffer[i + 1];
 				}
 				_LongBuffer[_LongBuffer.Length - 1] = iNewVal;
 
-				// Calculate mean over the buffer.
+				// Calculate the mean over the short buffer.
 				ShortAverage = (int)_ShortBuffer.Average();
 
-				// Calculate mean over the buffer.
+				// Calculate the mean over the long buffer.
 				LongAverage = (int)_LongBuffer.Average();
 
 			}
